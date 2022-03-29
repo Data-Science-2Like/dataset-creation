b225f96f1950048f5fc00a1b0996c2519601ead9
Simon Birkholz <simon.birkholz@uni-ulm.de>
2022-03-30 00:47:22 +0200

    added average age and count of citation

--- create_statistics.py
+++ create_statistics.py
@@ -2,6 +2,10 @@ from tqdm import tqdm
 import time
 from multiprocessing import Pool
 import json
+from statistics import mean
+from pathlib import Path
+from pebble import concurrent, ProcessPool, ProcessExpired
+import gzip
 
 # dataset.append({
 #                         'paper_id': metadata['paper_id'],
@@ -19,6 +23,71 @@ import json
 #                         'outgoing_citations_in_section' : cits_in_section
 #                     })
 
+FINE_GRAINED_LABELS = {
+     "introduction": 1, # Intro
+     "abstract": 2, # Abstract
+     "method": 3, # Methods
+     "methods": 3, # Methods
+     "results": 4, # Results
+     "discussion": 5, # Discussion
+     "discussions": 5, # Discussion
+     "conclusion": 6, # Conclusion
+     "conclusions": 6, # Conclusion
+     "results and discussion": 5, # Discussion
+     "related work": 7, # Background
+     "experimental results": 4, # Results
+     "literature review": 7, # Background
+     "experiments": 4, # Results
+     "background": 7, # Background
+     "methodology": 3, # Methods
+     "conclusions and future work": 6, # Conclusion
+     "related works": 7, # Background
+     "limitations": 5, # Discussion
+     "procedure": 3, # Methods
+     "material and methods": 3, # Methods
+     "discussion and conclusion": 5, # Discussion
+     "implementation": 3, # Methods
+     "evaluation": 4, # Results
+     "performance evaluation": 4, # Results
+     "experiments and results": 4, # Results
+     "overview": 1, # Introduction
+     "experimental design": 3, # Methods
+     "discussion and conclusions": 5, # Discussion
+     "results and discussions": 5, # Discussion
+     "motivation": 1, # Introduction
+     "proposed method": 3, # Methods
+     "analysis": 4, # Results
+     "future work": 6, # Conclusion
+     "results and analysis": 4, # Results
+     "implementation details": 3 # Methods 
+}
+
+NUM_TO_SEC = ["None","Introduction","Abstract","Methods","Results","Discussion","Conclusion", "Background"]
+
+
+def get_mapping(query: str) -> str:
+    return NUM_TO_SEC[FINE_GRAINED_LABELS[query]]
+
+
+def get_permissible_title_list():
+    with open('./permissible_section_titles.txt') as f:
+        titles = [l.strip() for l in f]
+    return titles
+
+def calc_avg(old_v, old_w, new_v, new_w = 1):
+    return (old_v * old_w + new_v * new_w) / (old_w + new_w)
+
+
+
+def get_years(ab):
+    p_years = {}
+    
+    with gzip.open(ab) as f:
+        for l in f:
+            entry = json.loads(l.strip())
+            p_years[entry['paper_id']] = entry['year']
+
+    return p_years
 
 if __name__ == "__main__":
 
@@ -26,6 +95,7 @@ if __name__ == "__main__":
     
 
     estimated_sec_count = 5567825
+    paper_years = {}
     paper_counts = {}
     paper_ids = {}
     total_paper_count = 0
@@ -33,13 +103,62 @@ if __name__ == "__main__":
     paper_section_counts = {}
     paper_ids_per_year = {}
 
-    with open(f"../data/citation_needed_data_contextualized_with_removal_v{version}.jsonl") as f:
+    sec_stats = {}
 
+    data_loc = Path('../20200705v1/full/')
+
+    files = [f for f in data_loc.glob(f"metadata/*.gz")]
+    with ProcessPool() as pool:
+        future = pool.map(get_years, files, timeout =300)
+        print("Collecting paper years")
+
+        iterator = future.result()
+        with tqdm(total=100) as pbar:
+            while True:
+                try:
+                    result = next(iterator)
+                    paper_years.update(result)
+
+                    pbar.update(1)
+                except StopIteration:
+                    break
+                except Exception as error:
+                    print(error.traceback)
+
+    print(f"Found {len(paper_years)} paper")
+    print("Collecting statistics")
+    with open(f"../data/citation_needed_data_contextualized_with_removal_v{version}.jsonl") as f:
         for line in tqdm(f, total=estimated_sec_count):
             entry = json.loads(line.strip())
             total_section_count += 1
             if entry['paper_id'] in paper_ids.keys():
                 paper_section_counts[entry['paper_id']] += 1
+
+                # collecting information abount average cits in sections and their age
+
+                st = get_mapping(entry['section_title'].lower())
+                if st not in sec_stats.keys():
+                    sec_stats[st] = {}
+                    sec_stats[st]['avg_count'] = 0
+                    sec_stats[st]['avg_age'] = 0
+                    sec_stats[st]['weight'] = 0
+
+                cit_count = len(list(filter(lambda s : s['label'] == 'check-worthy',entry['samples']))) / len(entry['samples'])
+
+                years = []
+                for s in entry['samples']:
+                    if 'ref_ids' in s.keys() and s['ref_ids'] is not None and len(s['ref_ids']) > 0:
+                        for id in s['ref_ids']:
+                            if id is not None and paper_years[id] is not None and entry['paper_year'] is not None:
+                                years.append(entry['paper_year'] - paper_years[id])
+
+                # avg_age = mean([entry['paper_year'] - paper_years[id] for s in entry['samples'] for id in s['ref_ids']])
+                avg_age = mean(years) if len(years) > 0 else 0
+                ow = sec_stats[st]['weight']
+                sec_stats[st]['avg_count'] = calc_avg(sec_stats[st]['avg_count'],ow, cit_count)
+                sec_stats[st]['avg_age'] = calc_avg(sec_stats[st]['avg_age'], ow, avg_age)
+                sec_stats[st]['weight'] += 1
+
                 continue
             # Only count each paper once
             paper_ids[entry['paper_id']] = True
@@ -75,3 +194,8 @@ if __name__ == "__main__":
 
     print(f"Total Paper Count: {total_paper_count}")
     print(f"Total Section Count: {total_section_count}")
+
+    print("-"*67)
+
+    for section in sec_stats.keys():
+        print(f"Section {section}: Avg Count: {sec_stats[section]['avg_count']} Avg Age: {sec_stats[section]['avg_age']}, Weight: {sec_stats[section]['weight']}")
